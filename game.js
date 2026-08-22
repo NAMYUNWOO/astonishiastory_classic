b52fca2949f86e5ccf858328b2659565848f5ee7
NAM YUNWOO <namyunwoo@icloud.com>
2026-08-22 11:51:42 +0900

    Update web build 2026-08-22 11:51

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ Module.expectedDataFileDownloads++;
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
     var PACKAGE_NAME = 'game.data';
-    var REMOTE_PACKAGE_BASE = 'game.data?v=20260822012406';
+    var REMOTE_PACKAGE_BASE = 'game.data?v=20260822115138';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       Module.printErr('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"a2e948da-d46d-4e24-844c-0a88304458cf","remote_package_size":36440235,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36440235,"audio":false}]});
+  loadPackage({"package_uuid":"6d55d6ea-8e9c-4bea-8b60-1e2cd61d0723","remote_package_size":36452498,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36452498,"audio":false}]});
 
 })();
